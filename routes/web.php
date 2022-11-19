d28ff92c55ceba4001222768a4c27f779bb87a93
Bima Tri Ariyanto <bima.jaya27@gmail.com>
2022-11-19 19:32:53 +0700

    auth and templating backend

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('backend.dashboard');
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/reload-captcha', [RegisterController::class, 'reloadCaptcha']);
